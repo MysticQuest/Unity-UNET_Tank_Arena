2d548d1e3038e7823aadabb19e0408d72cf518ec
Mystic Quest <l1skou@gmail.com>
2020-01-23 01:31:13 +0200

    movement input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(PlayerSetup))]
+[RequireComponent(typeof(PlayerShoot))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    PlayerShoot m_pShoot;
+    PlayerMotor m_pMotor;
+    PlayerSetup m_pSetup;
+    PlayerHealth m_pHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_pHealth = GetComponent<PlayerHealth>();
+        m_pMotor = GetComponent<PlayerMotor>();
+        m_pShoot = GetComponent<PlayerShoot>();
+        m_pSetup = GetComponent<PlayerSetup>();
+    }
+
+    Vector3 GetInput()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        return new Vector3(h, 0, v).normalized;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 inputDirection = GetInput();
+        m_pMotor.MovePlayer(inputDirection);
+    }
+
+    void Update()
+    {
+        Vector3 inputDirection = GetInput();
+        if (inputDirection.sqrMagnitude > 0.25f)  //sqrMag is to compare V3 to float
+        {
+            m_pMotor.RotateChassis(inputDirection);
+        }
+    }
+}
