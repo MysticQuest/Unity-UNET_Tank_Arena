ad5f6ef9030839dd6f64926affc92e9a08bb2cac
Mystic Quest <l1skou@gmail.com>
2020-01-23 04:11:42 +0200

    added labels

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine;
 
 public class PlayerSetup : NetworkBehaviour
 {
+    public Color m_playerColor;
+    public string m_baseName = "PLAYER";
+    public int m_playerNum = 1;
+    public Text m_playerNameText;
 
-    // Use this for initialization
-    void Start()
+    public override void OnStartClient()
     {
-
+        base.OnStartClient();
+        if (m_playerNameText != null)
+        {
+            m_playerNameText.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStartLocalPlayer()
     {
+        base.OnStartLocalPlayer();
+        MeshRenderer[] meshes = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer r in meshes)
+        {
+            r.material.color = m_playerColor;
+        }
 
+        if (m_playerNameText != null)
+        {
+            m_playerNameText.enabled = true;
+            m_playerNameText.text = m_baseName + m_playerNum.ToString();
+        }
     }
 }
