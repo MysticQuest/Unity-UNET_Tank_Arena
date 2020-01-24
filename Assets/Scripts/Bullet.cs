5d365108c6afed87598268192c21b922aa05e43d
Mystic Quest <l1skou@gmail.com>
2020-01-24 21:31:31 +0200

    added shooting, bullet mesh,vfx

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using System.Linq;
+
+public class Bullet : NetworkBehaviour
+{
+    public int m_speed = 100;
+
+    List<ParticleSystem> m_allParticles;
+
+    public float m_lifetime = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_allParticles = GetComponentsInChildren<ParticleSystem>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -6,15 +6,45 @@ using UnityEngine;
 public class PlayerShoot : NetworkBehaviour
 {
 
+    public Rigidbody m_bulletPrefab;
+    public Transform m_bulletSpawn;
+
+    public int m_shotsPerBurst = 2;
+    int m_shotsLeft;
+    bool m_isReloading;
+    public float m_reloadTime = 1f;
+
     // Use this for initialization
     void Start()
     {
-
+        m_shotsLeft = m_shotsPerBurst;
+        m_isReloading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
 
+    public void Shoot()
+    {
+        if (m_isReloading || m_bulletPrefab == null)
+        {
+            return;
+        }
+
+        Bullet bullet = null;
+        bullet = m_bulletPrefab.GetComponent<Bullet>();
+
+        Rigidbody rbody = Instantiate(m_bulletPrefab, m_bulletSpawn.position, m_bulletSpawn.rotation) as Rigidbody;
+
+        if (rbody != null)
+        {
+            rbody.velocity = bullet.m_speed * m_bulletSpawn.transform.forward;
+        }
     }
 }
