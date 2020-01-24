92d7be4960cddde02289699ba13d49b09fc07fed
Mystic Quest <l1skou@gmail.com>
2020-01-25 01:37:05 +0200

    game manager setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour
+{
+    static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject().AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+
+        set
+        {
+
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
